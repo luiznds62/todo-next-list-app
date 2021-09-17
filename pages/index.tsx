3b1add2cc0fb223e4055cc5eca3bd00c49680cd0
luiznds62 <luiznds62@gmail.com>
2021-09-16 23:22:14 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,16 @@ export default function Home() {
         integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
         crossOrigin="anonymous"
       ></Script>
+      <Script
+        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossOrigin="anonymous"
+      ></Script>
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossOrigin="anonymous"
+      ></Script>
 
       <Layout>
         <Sidenav></Sidenav>
