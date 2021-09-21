74d6bc6147c2913f8f3e8cc81cccf396dfcb45b3
luiznds62 <luiznds62@gmail.com>
2021-09-20 22:04:28 -0300

    Cria componente de botão

--- components/task-calendar/TaskCalendar.tsx
+++ components/task-calendar/TaskCalendar.tsx
@@ -1,5 +1,3 @@
-import styles from './TaskCalendar.module.scss';
-import AlertComponent from '../ui/alert/Alert';
 import ButtonComponent from '../ui/button/Button';
 
 export default function TaskCalendar() {
@@ -7,7 +5,39 @@ export default function TaskCalendar() {
     <>
       <div className="container">
         <h1>Almost before we knew it, we had left the ground.</h1>
-        <ButtonComponent></ButtonComponent>
+        {['primary', 'secondary', 'tertiary', 'success', 'danger', 'warning', 'dark'].map((variant, idx) => (
+          <div key={idx} className="row mb-2">
+            <div className="col">
+              <ButtonComponent key={idx} variant={variant} label="Button"></ButtonComponent>
+            </div>
+          </div>
+        ))}
+
+        {['round-primary', 'round-secondary', 'round-tertiary', 'round-success', 'round-danger', 'round-warning', 'round-dark'].map(
+          (variant, idx) => (
+            <div key={idx} className="row mb-2">
+              <div className="col">
+                <ButtonComponent key={idx} variant={variant} label="Button"></ButtonComponent>
+              </div>
+            </div>
+          )
+        )}
+
+        {[
+          'outline-primary',
+          'outline-secondary',
+          'outline-tertiary',
+          'outline-success',
+          'outline-danger',
+          'outline-warning',
+          'outline-dark'
+        ].map((variant, idx) => (
+          <div key={idx} className="row mb-2">
+            <div className="col">
+              <ButtonComponent key={idx} variant={variant} label="Button"></ButtonComponent>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,10 +1,14 @@
 import styles from './Button.module.scss';
 import { Button } from 'react-bootstrap';
 
-export default function ButtonComponent({ type, variant, onClick }: any) {
+export default function ButtonComponent({ variant, label, size, onClick }: any) {
+  function click() {
+    if (onClick) onClick();
+  }
+
   return (
-    <Button variant={variant} onClick={() => onClick()}>
-      Primary
+    <Button variant={variant} onClick={() => click()} size={size}>
+      {label}
     </Button>
   );
 }
