7118cbc57db323f3870ba63373b18ffecb25cf98
luiznds62 <luiznds62@gmail.com>
2021-09-20 22:27:00 -0300

    Botão semi-finalizado

--- components/task-calendar/TaskCalendar.tsx
+++ components/task-calendar/TaskCalendar.tsx
@@ -5,7 +5,7 @@ export default function TaskCalendar() {
     <>
       <div className="container">
         <h1>Almost before we knew it, we had left the ground.</h1>
-        <ButtonComponent variant={'primary'} label="Button" iconLeft="bi bi-plus"></ButtonComponent>
+        <ButtonComponent variant={'primary'} label="Button" iconLeft="bi bi-plus" disabled></ButtonComponent>
         <div className="mt-2">
           <ButtonComponent variant={'primary'} icon="bi bi-plus"></ButtonComponent>
         </div>
--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,13 +1,13 @@
 import styles from './Button.module.scss';
 import { Button } from 'react-bootstrap';
 
-export default function ButtonComponent({ variant, label, size, icon, iconLeft, iconRight, onClick }: any) {
+export default function ButtonComponent({ variant, label, size, icon, iconLeft, iconRight, disabled, onClick }: any) {
   function click() {
     if (onClick) onClick();
   }
 
   return (
-    <Button variant={variant} onClick={() => click()} size={size}>
+    <Button variant={variant} onClick={() => click()} size={size} disabled={disabled === 'true'}>
       {iconLeft && <i className={`${iconLeft} icon-left`}></i>}
       {icon && <i className={`${icon} icon-center`}></i>}
       {label}
