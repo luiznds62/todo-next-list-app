f50a614013c494837473841110b26d1704208b83
luiznds62 <luiznds62@gmail.com>
2021-09-21 22:53:01 -0300

    Criado avatars

--- components/task-calendar/TaskCalendar.tsx
+++ components/task-calendar/TaskCalendar.tsx
@@ -1,32 +1,34 @@
-import BadgeComponent from '../ui/badge/Badge';
-import ButtonComponent from '../ui/button/Button';
+import AvatarComponent from '../ui/avatar/Avatar';
 
 export default function TaskCalendar() {
   return (
     <>
       <div className="container">
         <h1>Almost before we knew it, we had left the ground.</h1>
-        <ButtonComponent variant={'primary'} label="Button" iconLeft="bi bi-plus" disabled></ButtonComponent>
-        <div className="mt-2">
-          <ButtonComponent variant={'primary'} icon="bi bi-plus"></ButtonComponent>
+
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" size="xl" />
         </div>
 
-        {[
-          'round-badge badge-primary',
-          'round-badge badge-secondary',
-          'round-badge badge-tertiary',
-          'round-badge badge-success',
-          'round-badge badge-warning',
-          'round-badge badge-danger',
-          'round-badge badge-info',
-          'round-badge badge-dark'
-        ].map((variant, idx) => {
-          return (
-            <div key={idx} className="mt-2">
-              <BadgeComponent variant={variant} label="New"></BadgeComponent>
-            </div>
-          );
-        })}
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" size="lg" />
+        </div>
+
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" />
+        </div>
+
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" size="sm" />
+        </div>
+
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" size="xs" />
+        </div>
+
+        <div className="mt-16">
+          <AvatarComponent name="Luiz Henrique" size="xss" />
+        </div>
       </div>
     </>
   );
---
+++ components/ui/avatar/Avatar.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+
+export default function AvatarComponent({ name, src, size }: any) {
+  const [initials, setInitials] = useState('');
+
+  useEffect(() => {
+    if (name.split(' ').length > 1) {
+      const tokens = name.split(' ');
+      const first = tokens[0].charAt(0);
+      const last = tokens[1].charAt(0);
+      setInitials(`${first}${last}`);
+    } else {
+      setInitials(name.charAt(0));
+    }
+  }, [name, initials]);
+
+  return <div className={`${src ? 'image-avatar' : 'default-avatar'} ${size && `avatar-${size}`}`}>{initials}</div>;
+}
