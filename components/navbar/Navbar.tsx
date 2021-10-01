8645d04458af6aa87bc61229848e816392a4d764
luiznds62 <luiznds62@gmail.com>
2021-09-30 22:09:53 -0300

    .

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,11 +2,33 @@ import ButtonComponent from '../ui/button/Button';
 import styles from './Navbar.module.scss';
 
 export default function Navbar() {
+  const today = new Date();
+
+  function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <ButtonComponent label="Today" />
+        <div className={styles.leftPart}>
+          <ButtonComponent label="Today" variant="outline-secondary" />
+          <div className={styles.leftRightIcons}>
+            <i className="bi bi-arrow-left-circle"></i>
+            <i className="bi bi-arrow-right-circle"></i>
+          </div>
+        </div>
+        <div className={styles.todayDate}>
+          <span className="m-text dark-text text-600">Today</span>
+          <span className="m-text text-muted text-600">
+            {today.getDate()}th {capitalizeFirstLetter(today.toLocaleString('default', { month: 'long' }))}
+          </span>
+        </div>
+        <div>
+          <ButtonComponent label="Refresh" variant="outline-secondary" iconRight="bi bi-arrow-clockwise" />
+        </div>
       </div>
+      <div className={styles.userContainer}>user</div>
     </div>
   );
 }
